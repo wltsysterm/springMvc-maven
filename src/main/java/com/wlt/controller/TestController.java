6eb82ad0f806f0776b61b37864b0674c2353325e
魏霖涛 <3263138624@qq.com>
2017-08-23 14:01:43 +0800

    log4j.properties使用

--- src/main/java/com/wlt/controller/TestController.java
+++ src/main/java/com/wlt/controller/TestController.java
@@ -1,16 +1,23 @@
 package com.wlt.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class TestController {
+    //1、以当前类.class初始化，会自动将类的全路径作为参数初始化log，那么过滤条件就是以该全路径作为条件，跟配置文件里面的配置做比较
+//    private Logger logger = Logger.getLogger(TestController.class);
+    //2、也可以手动设置路径字符串
+    private Logger logger = Logger.getLogger("com.wlt.controller.TestController");
     @RequestMapping("do")
     @ResponseBody
     public void doSomething(){
         System.out.println("11111");
+        logger.error("hahah");
     }
 
     @RequestMapping("do1")
